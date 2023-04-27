51e2b353abe84e3b4007a974951f69e6c42de21c
trak <trakxyz@tuta.io>
2023-04-27 18:19:38 +0000

    ascii

--- new/weafetch.py
+++ new/weafetch.py
@@ -3,28 +3,32 @@ from requests import get
 from itertools import zip_longest
 
 
-city: str = "Novska"
-url: str = f"http://wttr.in/{city}?format=j1"
-
-error_internet: str = "There was a problem while fetching the data, error code:"
+error_internet: str = "There seems to be a problem with internet."
 common_space = 3
 
 
-init = get(url)
-getip = get("https://ipinfo.io/json")
+neutral = """   .--.
+ .-(    ).
+(___.__)__)"""
+
+cloud = """  .-.
+ (   ). 
+(___(__)"""
+
+rain = """  ‘ ‘ ‘ ‘
+‘ ‘ ‘ ‘"""
 
 
-if init.status_code != 200 or getip.status_code != 200:
-    raise Exception(error_internet)
+try: getip = get("https://ipinfo.io/json")
+except Exception as e: raise SystemExit(error_internet)
 
 city: str = loads(getip.text)["city"]
+url: str = f"http://wttr.in/{city}?format=j1"
+
+try: init = get(url)
+except Exception as e: raise SystemExit(error_internet)
+
 weatherRaw: dict = loads(init.text)
-
-
-WHITE, END = "", ""
-neutral = f"""{WHITE}    .--.  {END}
-{WHITE} .-(    ).{END}
-{WHITE}(___.__)__){END}"""
 
 
 def getWeather():
@@ -40,6 +44,8 @@ def getWeather():
         "desc": currentWeather["weatherDesc"][0]["value"],
         "wind_speed": currentWeather["windspeedKmph"],
         "wind_direction": currentWeather["winddir16Point"],
+        "city": weatherRaw["nearest_area"][0]["areaName"][0]["value"],
+        "country": weatherRaw["nearest_area"][0]["country"][0]["value"]
     }
 
     windDirection = weather["wind_direction"]
@@ -56,23 +62,39 @@ def getWeather():
 
     global descriptors
     descriptors = [
+        f'{weather["city"]}, {weather["country"]}',
         weather["desc"],
-        f'{weather["feels_like"]}({weather["temp"]})C',
-        f'{weather["rain_chance"]}% | {weather["rain_levels"]}mm',
-        f'{weather["wind_speed"]}km/h | {wind}',
+        f'{weather["feels_like"]}({weather["temp"]})°C',
+        f'{weather["rain_chance"]}% | {weather["rain_levels"]} mm',
+        f'{weather["wind_speed"]}km/h {wind}',
     ]
 
 
+def getArt(art: str = "") -> str:
+
+    desc = weather.get("desc", None).lower()
+
+    if "cloud" in desc:
+        art += cloud + "\n"
+    if "rain" in desc:
+        art += rain + "\n"
+    else:
+        art = neutral
+
+    return art
+
 def main() -> None:
 
-    longest: int = max([len(x) for x in neutral.splitlines()])
-    for art, desc in zip_longest(neutral.splitlines(), descriptors, fillvalue=""):
+    artFull: str = getArt()
+
+    longest: int = max([len(x) for x in artFull.splitlines()])
+    for art, desc in zip_longest(artFull.splitlines(), descriptors, fillvalue=""):
 
         print(art, " " * (longest - len(art)), end=" " * common_space)
-
         print(desc)
 
 
 if __name__ == "__main__":
     getWeather()
     main()
+    print()
