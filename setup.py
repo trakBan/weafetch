d4aed8439bce52285f2c0e8bf1bc1062e7be6d78
trak <trakxyz@tuta.io>
2023-05-22 20:41:17 +0000

    ~/.config/weafetch config and ascii

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from setuptools import setup
+from os import environ, mkdir
+from shutil import copy
+
+
+mkdirError = "You have to create ~./config/weafetch/ folder yourself"
+copyError = "You have to mannualy copy.json config and artascii.py to ~/.config/weafetch/"
+
+
+path = f"{environ['HOME']}/.config/weafetch/"
+print(path)
+
+try: mkdir(path)
+except Exception as e: raise SystemError(mkdirError)
+
+try:
+    copy("artascii.py", path)
+    copy("config.json", path)
+
+except Exception as e: raise SystemError(copyError)
 
 
 with open("README.md", "r") as f:
