08ef495f0f4d638985ba79ca3d939b4473cb30d9
trak <trakxyz@tuta.io>
2023-05-24 20:26:25 +0000

    artascii added as an another file

--- setup.py
+++ setup.py
@@ -1,42 +1,37 @@
 from setuptools import setup
-from os import environ, mkdir
-from shutil import copy
+#from os import mkdir, path
+#from shutil import copy
 
-
+"""
 mkdirError = "You have to create ~./config/weafetch/ folder yourself"
-copyError = "You have to mannualy copy.json config and artascii.py to ~/.config/weafetch/"
+copyError = (
+    "You have to mannualy copy src/config.json to ~/.config/weafetch/"
+)
 
-
-path = f"{environ['HOME']}/.config/weafetch/"
-print(path)
-
-try: mkdir(path)
-except Exception as e: raise SystemError(mkdirError)
+path = f"{path.expanduser('~')}/.config/weafetch"
 
 try:
-    copy("artascii.py", path)
-    copy("config.json", path)
+    mkdir(path)
 
-except Exception as e: raise SystemError(copyError)
+except FileExistsError:
+    raise SystemExit("The folder ~/.config/weafetch already exists")
 
+except Exception as e:
+    raise SystemError(e, mkdirError)
+
+try: copy("src/config.json", path)
+except Exception as e: raise SystemExit(copyError, e)
 
 with open("README.md", "r") as f:
     desc = f.read()
-
+"""
 
 setup(
-
     author="trakBan",
     description="See the weather in terminal, neofetch style",
     long_description=desc,
     name="weafetch",
     version="0.01a",
-
-    install_requires=[
-        "requests",
-        "argparse"
-    ],
-    scripts=[
-        "weafetch"
-    ]
+    install_requires=["requests", "argparse"],
+    scripts=["weafetch", "src/artascii.py"],
 )
