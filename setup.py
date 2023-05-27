cc656b4f310a0dd79b0ecde0e6160d44ada1aaca
trak <trakxyz@tuta.io>
2023-05-28 01:55:17 +0200

    json config file setup

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 from setuptools import setup
-#from os import mkdir, path
-#from shutil import copy
+from os import mkdir, path, environ
+from shutil import copy
 
-"""
+
 mkdirError = "You have to create ~./config/weafetch/ folder yourself"
-copyError = (
-    "You have to mannualy copy src/config.json to ~/.config/weafetch/"
-)
+copyError = "You have to mannualy copy src/config.json to ~/.config/weafetch/"
+fileExistsError = "The folder ~/.config/weafetch already exists, delete it or move config.json mannualy"
+sudoUserError = "There has been a problem with fetching your username, USERNAME/.local/weafetch enter it mannualy: "
 
-path = f"{path.expanduser('~')}/.config/weafetch"
 
-try:
-    mkdir(path)
+try: user: str = environ.get("SUDO_USER")
+except: user: str = input(sudoUserError)
 
-except FileExistsError:
-    raise SystemExit("The folder ~/.config/weafetch already exists")
+path: str = f"/home/{user}/.config/weafetch"
 
-except Exception as e:
-    raise SystemError(e, mkdirError)
+try: mkdir(path)
+except FileExistsError: raise SystemExit(fileExistsError)
+except Exception as e: raise SystemError(e, mkdirError)
 
 try: copy("src/config.json", path)
 except Exception as e: raise SystemExit(copyError, e)
-"""
+
+
 with open("README.md", "r") as f:
     desc = f.read()
+
 
 setup(
     author="trakBan",
