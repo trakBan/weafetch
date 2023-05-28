ef9c621848c09bff9c09af9a62f1372662d65197
trak <trakxyz@tuta.io>
2023-05-28 13:05:00 +0200

    fixed rendering problem with ANSII

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ copyError = "You have to mannualy copy src/config.json to ~/.config/weafetch/"
 fileExistsError = "The folder ~/.config/weafetch already exists, delete it or move config.json mannualy"
 sudoUserError = "There has been a problem with fetching your username, USERNAME/.local/weafetch enter it mannualy: "
 
-
+"""
 try: user: str = environ.get("SUDO_USER")
 except: user: str = input(sudoUserError)
 
@@ -20,7 +20,7 @@ except Exception as e: raise SystemError(e, mkdirError)
 
 try: copy("src/config.json", path)
 except Exception as e: raise SystemExit(copyError, e)
-
+"""
 
 with open("README.md", "r") as f:
     desc = f.read()
